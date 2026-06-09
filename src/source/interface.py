67549ab849fb27cf430abf4eedf94d04c4989c2b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-06-09 21:28:02 +0100

    Update src/source/interface.py

--- src/source/interface.py
+++ src/source/interface.py
@@ -1,7 +1,5 @@
 """Module interface.py"""
 
-import logging
-
 import numpy as np
 import pandas as pd
 
@@ -17,7 +15,7 @@ class Interface:
 
     def __init__(self, clients: dict):
 
-        self.__url = f's3://{clients.get('internal')}/companies/data/all/196*-01-01.csv'
+        self.__url = f's3://{clients.get('internal')}/companies/data/all/*.csv'
 
     def exc(self, cycle: str):
         """
@@ -28,14 +26,6 @@ class Interface:
 
         # the data
         data: pd.DataFrame = src.source.data.Data(url=self.__url).exc()
-        data.info()
-        logging.info(data)
-
-        # x = pd.NaT if data['date_of_cessation'].isnull() else data['date_of_cessation'].astype(np.int64) / 1e6
-        logging.info(data['date_of_cessation'].where(
-            data['date_of_cessation'].isnull(),
-            data['date_of_cessation'].astype(np.int64) / 1e6)
-        )
 
         # setting granularity per instance
         data = src.source.sample.Sample(data=data.copy(), cycle=cycle).exc()
