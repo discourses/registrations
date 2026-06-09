6d6ccf188a75ba7dbdae1b6c44383ca587039b3e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-06-09 20:09:42 +0100

    Add src/distributions/persist.py

---
+++ src/distributions/persist.py
@@ -0,0 +1,30 @@
+"""Module persist"""
+
+import os
+
+import config
+import src.functions.objects
+
+
+class Persist:
+
+    def __init__(self):
+        """
+
+        Constructor
+        """
+
+        self.__configurations = config.Config()
+        self.__objects = src.functions.objects.Objects()
+
+
+    def __call__(self, patterns: dict | list[dict], section: str):
+        """
+
+        :param patterns:
+        :param section:
+        :return:
+        """
+
+        return self.__objects.write(
+            nodes=patterns, path=os.path.join(self.__configurations.distributions_, f'{section}.json'))
